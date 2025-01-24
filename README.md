79bcd3496c6b1ed8c2c3cd5f3f38570eaa1be4a2
KareemMusleh <prototribe@protonmail.com>
2025-01-25 00:17:21 +0700

    readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# RISCV
+implementing RV64I to pass the [riscv-tests](https://github.com/riscv-software-src/riscv-tests)
--- cpu.py
+++ cpu.py
@@ -7,7 +7,7 @@ BASE_ADDRESS = 0x80000000
 if __name__ == "__main__":
     # implementing RV64 user-level, integer only
     # virtual memory is disabled, only core 0 boots up
-    for file in glob.glob('riscv-tests/isa/rv32ui-p*'):
+    for file in glob.glob('riscv-tests/isa/rv64ui-p*'):
         if file.endswith('.dump'): continue
         with open(file, 'rb') as f:
             print(f'test {file}: ', end='\n')
